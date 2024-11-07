368dd59dd381f9376b67188200cd5a18a3decc57
pretam3868 <163520993+pretam3868@users.noreply.github.com>
2024-11-07 17:12:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const modal = document.getElementById("myModal");
+    const openModalBtn = document.getElementById("openModal");
+    const closeModalBtn = document.getElementsByClassName("close-modal")[0];
+
+    // When the user clicks the button, open the modal
+    openModalBtn.onclick = function () {
+      modal.style.display = "block";
+    };
